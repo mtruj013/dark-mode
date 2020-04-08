0b0da2fb0a9ba7ecbee42b543450b2eba6eeb719
Maria Trujillo <mptruj015@gmail.com>
2020-04-09 00:09:58 +0200

    reached mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,17 @@
+import {useEffect} from 'react';
+import {useLocalStorage} from "./useLocalStorage";
+
+export const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useLocalStorage('key');
+
+    useEffect(() => {
+        if(darkMode === true){
+            document.body.classList.add('dark-mode');
+        }
+        else{
+            document.body.classList.remove('dark-mode');
+        }
+    },[darkMode])
+    
+    return [darkMode, setDarkMode];
+}
